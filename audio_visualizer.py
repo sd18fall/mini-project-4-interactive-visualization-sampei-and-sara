76818ee4e2f1f18ba104b4f4b76264bd14d1b921
Sampei Omichi <sampeiomichi@gmail.com>
2018-10-29 11:51:46 -0400

    Updated MP4

--- audio_visualizer.py
+++ audio_visualizer.py
@@ -45,20 +45,60 @@ def process_key(key):
     """
     print(chr(key))
 
+def bpm_to_frame(bpm):
+    bps = 60 / bpm
+    frames = bps * 24
+    return frames
+
+def key_q(key, frames):
+    for frame in range(1,frames):
+        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie1_'+str(frame)+'.png')
+        gameDisplay.blit(to_display, (0,0))
+        pygame.time.delay(24)
+        pygame.display.update()
+
+def key_w(key, frames):
+    for frame in range(1,frames):
+        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie2_'+str(frame)+'.png')
+        gameDisplay.blit(to_display, (0,0))
+        pygame.time.delay(24)
+        pygame.display.update()
+
+def key_e(key, frames):
+    for frame in range(1,frames):
+        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie3_'+str(frame)+'.png')
+        gameDisplay.blit(to_display, (0,0))
+        pygame.time.delay(24)
+        pygame.display.update()
+
 if __name__ == "__main__": #Controller
-    # gameDisplay = pygame.display.set_mode((1,1))
-    # clock = pygame.time.Clock()
-    # end = False
-    # while not end:
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.KEYDOWN:
-    #             if event.key == ord('q'):
-    #                 end = True
-    #             else:
-    #                 process_key(event.key)
-    #     pygame.display.update()
-    #     clock.tick(30)
+
     WAV_FILE = '/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/pyAudioAnalysis/data/beat/small.wav'
     [Fs, x] = audioBasicIO.readAudioFile(WAV_FILE);
     F, f_names = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
-    print(audioFeatureExtraction.beatExtraction(F, 1, PLOT=True))
+    print(audioFeatureExtraction.beatExtraction(F, 1, PLOT=False))
+
+    frames = 239
+
+    pygame.init()
+    display_width = 350
+    display_height = 350
+
+    gameDisplay = pygame.display.set_mode((display_width,display_height))
+    end = False
+    while not end:
+        key = 'x'
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                key = event.key
+        if key == ord('z'):
+            end = True
+        if key == ord('q'):
+            print('Regular')
+            key_q(key, frames)
+        if key == ord('w'):
+            print('Colors Inverted!')
+            key_w(key, frames)
+        if key == ord('e'):
+            print('Image Greyscaled!')
+            key_e(key, frames)
--- interactive_framework.py
+++ interactive_framework.py
@@ -13,41 +13,36 @@ from pyAudioAnalysis import audioFeatureExtraction
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioAnalysis
 
-def process_key(key):
-    """ Controller function - identifies key input
-    """
-    if key == ord('q'):
-        print('Regular')
-        key_q()
-    if key == ord('w'):
-        print('Colors Inverted!')
-        key_w()
-    if key == ord('e'):
-        print('Image Greyscaled!')
-        key_e()
 
-def key_q():
-    for frame in range(1,239):
+def bpm_to_frame(bpm):
+    bps = 60 / bpm
+    frames = bps * 24
+    return frames
+
+def key_q(key, frames):
+    for frame in range(1,frames):
         to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie1_'+str(frame)+'.png')
         gameDisplay.blit(to_display, (0,0))
         pygame.time.delay(24)
         pygame.display.update()
 
-def key_w():
-    for frame in range(1,239):
+def key_w(key, frames):
+    for frame in range(1,frames):
         to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie2_'+str(frame)+'.png')
         gameDisplay.blit(to_display, (0,0))
         pygame.time.delay(24)
         pygame.display.update()
 
-def key_e():
-    for frame in range(1,239):
+def key_e(key, frames):
+    for frame in range(1,frames):
         to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie3_'+str(frame)+'.png')
         gameDisplay.blit(to_display, (0,0))
         pygame.time.delay(24)
         pygame.display.update()
 
-if __name__ == "__main__": #Controller
+if __name__ == "__main__": # Controller
+
+    frames = int(bpm_to_frame(60))
 
     pygame.init()
     display_width = 350
@@ -56,9 +51,18 @@ if __name__ == "__main__": #Controller
     gameDisplay = pygame.display.set_mode((display_width,display_height))
     end = False
     while not end:
+        key = 'x'
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == ord('z'):
-                    end = True
-                else:
-                    process_key(event.key)
+                key = event.key
+        if key == ord('q'):
+            print('q')
+        if key == ord('h'):
+            print('h')
+
+                # while key == ord('w'):
+                #     print('Colors Inverted!')
+                #     key_w(key, frames)
+                # while key == ord('e'):
+                #     print('Image Greyscaled!')
+                #     key_e(key, frames)
