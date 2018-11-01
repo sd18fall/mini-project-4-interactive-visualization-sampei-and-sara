ffedc5291252b886139d8f52c67ae33df6a4d0a3
Sampei Omichi <sampeiomichi@gmail.com>
2018-10-31 22:15:03 -0400

    Added doc strings

--- audio_visualizer.py
+++ audio_visualizer.py
@@ -23,8 +23,6 @@ from pyAudioAnalysis import audioFeatureExtraction
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioAnalysis
 
-
-
 __author__ = 'Sampei and Sara'
 __version__ = '0.0.1'
 
@@ -46,41 +44,42 @@ def process_key(key):
     print(chr(key))
 
 def bpm_to_frame(bpm):
+    '''Gets the required number of frames to loop given the average beats per minute from a song'''
     bps = 60 / bpm
     frames = bps * 24
     return frames
 
 def key_q(key, frames, x, y):
+    '''Plays the frames with the regular color scheme'''
     for frame in range(1,frames):
-        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie1_'+str(frame)+'.png')
+        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie1_'+str(frame)+'.png') #This hardcoded line would have to be changed
         gameDisplay.blit(to_display, (x,y))
         pygame.time.delay(24)
         pygame.display.update()
 
 def key_w(key, frames, x, y):
+    '''Plays the frames with the inverted color scheme'''
     for frame in range(1,frames):
-        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie2_'+str(frame)+'.png')
+        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie2_'+str(frame)+'.png') #This hardcoded line would have to be changed
         gameDisplay.blit(to_display, (x,y))
         pygame.time.delay(24)
         pygame.display.update()
 
 def key_e(key, frames, x, y):
+    '''Plays the frames with the greyscaled color scheme'''
     for frame in range(1,frames):
-        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie3_'+str(frame)+'.png')
+        to_display = pygame.image.load('/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/ComputationalArt-sampeiomichi/movie3_'+str(frame)+'.png') #This hardcoded line would have to be changed
         gameDisplay.blit(to_display, (x,y))
         pygame.time.delay(24)
         pygame.display.update()
 
-if __name__ == "__main__": #Controller
-    # my_path = os.path.abspath(os.path.dirname(__file__))
-    # WAV_FILE = os.path.join(my_path, 'mini-project-4-interactive-visualization-sampei-and-sara/pyAudioAnalysis/data/beat/small.wav')
-    WAV_FILE = '/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/pyAudioAnalysis/data/beat/small.wav'
-    [Fs, x] = audioBasicIO.readAudioFile(WAV_FILE);
-    F, f_names = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs);
-    bpm, ratio = audioFeatureExtraction.beatExtraction(F, 1, PLOT=False)
-
-    frames = int(bpm_to_frame(int(bpm)*2))
+if __name__ == "__main__": #Main
+    file = '/home/sampeiomichi/mini-project-4-interactive-visualization-sampei-and-sara/pyAudioAnalysis/data/beat/insideout.mp3' #This hardcoded line would have to be changed
+    frames = int(bpm_to_frame(60)) #Gets the number of frames that need to be looped given 60 BPM
     pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
     display_width = 350
     display_height = 350
 
@@ -93,39 +92,39 @@ if __name__ == "__main__": #Controller
     while not end:
         input = ''
         key = ''
-        for event in pygame.event.get():
+        for event in pygame.event.get(): #Takes a keystroke as an input
             if event.type == pygame.KEYDOWN:
                 key = event.key
-        if key == ord('z'):
+        if key == ord('z'): #Quits the program when 'z' is pressed
             end = True
-        elif key == ord('q'):
+        elif key == ord('q'): #Changes the state to the regular color scheme
             print('Regular')
             state = 1
-        elif key == ord('w'):
+        elif key == ord('w'): #Changes the state to the inverted color scheme
             print('Colors Inverted!')
             state = 2
-        elif key == ord('e'):
+        elif key == ord('e'): #Changes the state to the greyscaled color scheme
             print('Image Greyscaled!')
             state = 3
-        elif key == 273:
+        elif key == 273: #Moves the screen upwards when up key is pressed
             if y >= -650 and y < 0:
                 print('Up')
                 y = y + 50
-        elif key == 274:
+        elif key == 274: #Moves the screen downwards when down key is pressed
             if y > -650 and y <= 0:
                 print('Down')
                 y = y - 50
-        elif key == 276:
+        elif key == 276: #Moves the screen left when left key is pressed
             if x >= -650 and x < 0:
                 print('Left')
                 x = x + 50
-        elif key == 275:
+        elif key == 275: #Moves the screen right when right key is pressed
             if x > -650 and x <= 0:
                 print('Right')
                 x = x - 50
-        if state == 1:
+        if state == 1: #Loops over the regular color scheme
             key_q(key, frames, x, y)
-        elif state == 2:
+        elif state == 2: #Loops over the inverted color scheme
             key_w(key, frames, x, y)
-        elif state == 3:
+        elif state == 3: #Loops over the greyscaled color scheme
             key_e(key, frames, x, y)
